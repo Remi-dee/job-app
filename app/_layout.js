a60911506a415e213645814496d78f1fe33571f5
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2024-03-24 20:31:28 +0000

    Sunday, March 24, 2024 at 9:31 PM West Africa Standard Time

--- app/_layout.js
+++ app/_layout.js
@@ -1,13 +1,29 @@
 import { Stack } from "expo-router";
 import { useCallback } from "react";
 
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
 
 function _layout() {
-  return (
-    <Stack initialRouteName="home">
-      <Stack.Screen name="home" />
-    </Stack>
-  );
+  const [fontsLoaded] = useFonts({
+    DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+    DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+    DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return <Stack onLayoutRootView={onLayoutRootView} initialRouteName="home" />;
 }
 
 export default _layout;
